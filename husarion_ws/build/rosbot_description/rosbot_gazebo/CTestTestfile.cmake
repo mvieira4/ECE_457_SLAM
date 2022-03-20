7caaa3e899ef4b0e6c2d3493d500f699355fd0ad
rsheran1 <rsheran@umassd.edu>
2022-03-20 17:58:47 +0000

    Added gitignore for build/devel files

--- husarion_ws/build/rosbot_description/rosbot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vagrant/ECE_457_SLAM/husarion_ws/src/rosbot_description/rosbot_gazebo
-# Build directory: /home/vagrant/ECE_457_SLAM/husarion_ws/build/rosbot_description/rosbot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- husarion_ws/build/rosbot_ekf/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/vagrant/ECE_457_SLAM/husarion_ws/devel/include".split(';') if "/home/vagrant/ECE_457_SLAM/husarion_ws/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "rosbot_ekf"
-PROJECT_SPACE_DIR = "/home/vagrant/ECE_457_SLAM/husarion_ws/devel"
-PROJECT_VERSION = "1.0.0"
