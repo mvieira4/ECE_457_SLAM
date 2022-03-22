ce3b456030cb10ec4865a37c16f61e6a159d0122
mvieira4 <47149053+mvieira4@users.noreply.github.com>
2022-03-22 13:56:50 -0400

    fix[husarion_ws]: removed build dir

--- husarion_ws/build/pose_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/ECE_457_SLAM/husarion_ws/src/pose_pkg
-# Build directory: /home/ros/ECE_457_SLAM/husarion_ws/build/pose_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
